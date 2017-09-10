58c9ee4a29ddf6df4618edbbdc62d77e82d73c71
scosacescu <stephanie.cosacescu@gmail.com>
2017-09-10 16:53:20 -0700

    Final commit for lab01

--- index.php
+++ index.php
@@ -11,7 +11,48 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hello out there... I live.";
+        include('Student.php');
+
+        //init an array to hold Students
+        $students = array();
+        
+        //populate the array
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+        //author added to Student array
+        $third = new Student();
+        $third->surname = "Cosacescu";
+        $third->first_name = "Stephanie";
+        $third->add_email('home','stephanie.cosacescu@gmail.com');
+        $third->add_email('work','icosacescu@my.bcit.ca');
+        $third->add_grade(95);
+        $students['s123'] = $third;
+
+        //alphabetize the array before printing
+        ksort($students); // one of the many sort functions
+        
+        foreach ($students as $student) {
+            echo $student->toString();
+        }
         ?>
     </body>
 </html>
